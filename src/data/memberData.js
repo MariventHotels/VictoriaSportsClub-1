bae44c7f54620f643d22b1ef67660667e7b0c850
Thanaxx <christeljoy.abordaje@isatu.edu.ph>
2023-04-09 16:49:47 +0800

    membership

--- src/data/memberData.js
+++ src/data/memberData.js
@@ -1,24 +1,24 @@
 const memberData = [
     {
-        memberImg: '/vscimages/membership/kidie.jpeg',
+        memberImg: '/vscimages/membership/AnnualPremiere.webp',
         memberTitle: "ANNUAL PREMIERE",
         memberDesc: "With flexible payment terms, get all-year-round access to Victoria Sports Club Fitness Gym, Billiards, and Water Activities. Includes Sauna, Jacuzzi, and Steam Room. Basketball, Tennis, and Badminton Courts are included but are subject to court fees. ",
     },
 
     {
-        memberImg: '/vscimages/membership/Tennis mom.jpeg',
+        memberImg: '/vscimages/membership/02 Semi Annual.webp',
         memberTitle: "SEMI-ANNUAL PREMIERE",
         memberDesc: "Enjoy 6-month access to Victoria Sports Club Fitness Gym, Billiards, and Water Activities. Includes Sauna, Jacuzzi, and Steam Room. Basketball, Tennis, and Badminton Courts are included but are subject to court fees.",
     },
 
     {
-        memberImg: '/vscimages/membership/01 Annual Premiere.webp',
+        memberImg: '/vscimages/membership/Gym.webp',
         memberTitle: "GYM ACCESS",
         memberDesc: "Secure one (1) month of full access to Victoria Sports Club Gym.",
     },
 
     {
-        memberImg: '/vscimages/membership/basketball edit.jpg',
+        memberImg: '/vscimages/membership/GymAccess.webp',
         memberTitle: "GYM ACCESS + 1",
         memberDesc: "Get one (1) month membership to Victoria Sports Club and get access to Victoria Sports Club Fitness Gym and one (1) of its amenities. Choose from: Swimming Pool; Tennis Court; Basketball Court; or Billiards.  ",
     },
